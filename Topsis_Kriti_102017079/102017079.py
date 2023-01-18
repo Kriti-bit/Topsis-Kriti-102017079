34ed1007cb806977876d165002b9232685e3c60e
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2023-01-18 21:46:52 +0530

    removed bugs

--- Topsis_Kriti_102017079/102017079.py
+++ Topsis_Kriti_102017079/102017079.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
 
 
 def main():
@@ -32,13 +33,13 @@ def main():
         exit()
 
     # Error if weights are not equal to number of columns
-    if len(sys.argv[2].split(",")) != len(pd.read_csv(sys.argv[1]).columns):
+    if len(sys.argv[2].split(",")) != len(pd.read_csv(sys.argv[1]).columns) - 1:
         print("ERROR: Number of weights not equal to number of columns")
         exit()
 
     # Error if weights are not separated by comma
     try:
-        weights = [float(i) for i in sys.argv[2].split(",")]
+        weights = [int(i) for i in sys.argv[2].split(",")]
     except ValueError:
         print("ERROR: Weights are not provided in the correct format")
         exit()
@@ -49,7 +50,7 @@ def main():
         exit()
 
     # Error if impacts are not equal to number of columns
-    if len(sys.argv[3].split(",")) != len(pd.read_csv(sys.argv[1]).columns):
+    if len(sys.argv[3].split(",")) != len(pd.read_csv(sys.argv[1]).columns) - 1:
         print("ERROR: Number of impacts not equal to number of columns")
         exit()
 
@@ -71,12 +72,12 @@ def main():
     data = pd.read_csv(sys.argv[1])
 
     # Error If number of columns is less than 3
-    if len(data.columns) < 3:
+    if len(data.columns)-1 <= 3:
         print("ERROR: Number of columns is less than 3")
         exit()
 
     # 2nd column of input file has non numeric values then encode it
-    for i in range(1, len(data.columns)):
+    for i in range(1, len(data.columns)-1):
         pd.to_numeric(data.iloc[:, i], errors="coerce")
         data.iloc[:, i].fillna((data.iloc[:, i].mode()), inplace=True)
 
@@ -89,68 +90,85 @@ def topsis(data, result_file, weights, impact):
     new_data = weighted_sum(new_data, weights)
     best_solution = ideal_best_solution(new_data, impact)
     worst_solution = ideal_worst_solution(new_data, impact)
-    euclidean_distances = calc_euclidean_distance(
+    scores = calc_score(
         new_data, best_solution, worst_solution)
-    new_data["Score"] = euclidean_distances
+    new_data["Score"] = scores
     new_data["Rank"] = new_data["Score"].rank(method="max", ascending=False)
     new_data = new_data.astype({"Rank": int})
     new_data.to_csv(result_file, index=False)
 
 
-def calc_euclidean_distance(data, best_solution, worst_solution):
+def calc_score(data, best_solution, worst_solution):
     # Euclidean distance
-    euclidean_distances = []
+    scores = []
     for i in range(len(data)):
         pos_euclidean_distance = 0
         neg_euclidean_distance = 0
-        for j in data.columns:
-            pos_euclidean_distance += (data[j][i] - best_solution[j][i])**2
-            neg_euclidean_distance += (data[j][i] - worst_solution[j][i])**2
+        for j in range(1, data.columns.values.size):
+            pos_euclidean_distance += (best_solution[j-1] - data.iloc[i, j])**2
+            neg_euclidean_distance += (
+                worst_solution[j-1] - data.iloc[i, j])**2
         pos_euclidean_distance, neg_euclidean_distance = pos_euclidean_distance**0.5, neg_euclidean_distance**0.5
-        euclidean_distance = (neg_euclidean_distance /
-                              (pos_euclidean_distance + neg_euclidean_distance))
-        euclidean_distances.append(euclidean_distance)
+        score = (neg_euclidean_distance /
+                 (pos_euclidean_distance + neg_euclidean_distance))
+        scores.append(score)
 
-    return euclidean_distances
+    return scores
 
 
 def ideal_best_solution(data, impact):
     # Ideal best solution
-    ideal_solution_data = data.copy()
-    for i in data.columns:
-        if impact[i] == "+":
-            ideal_solution_data[i] = max(data[i])
+    ideal_solution_data = []
+    for i in range(1, len(data.columns.values)):
+        if impact[i-1] == "+":
+            ideal_solution_data.append(max(data.iloc[:, i]))
         else:
-            ideal_solution_data[i] = min(data[i])
-
+            ideal_solution_data.append(min(data.iloc[:, i]))
+    ideal_solution_data = np.array(ideal_solution_data)
     return ideal_solution_data
+
+    # for i in range(1, len(data.columns.values)):
+    #     if impact[i-1] == "+":
+    #         ideal_solution_data[i-1] = max(data[:i])
+    #     else:
+    #         ideal_solution_data[i-1] = min(data[:i])
+    # ideal_solution_data = np.array(ideal_solution_data)
+    # return ideal_solution_data
 
 
 def ideal_worst_solution(data, impact):
     # Ideal worst solution
-    ideal_solution_data = data.copy()
-    for i in data.columns:
-        if impact[i] == "+":
-            ideal_solution_data[i] = min(data[i])
+    ideal_solution_data = []
+    for i in range(1, len(data.columns.values)):
+        if impact[i-1] == "+":
+            ideal_solution_data.append(min(data.iloc[:, i]))
         else:
-            ideal_solution_data[i] = max(data[i])
-
+            ideal_solution_data.append(max(data.iloc[:, i]))
+    ideal_solution_data = np.array(ideal_solution_data)
     return ideal_solution_data
+    # for i in range(0, len(data.columns.values)-1):
+    #     if impact[i] == "+":
+    #         ideal_solution_data[i] = min(data[i])
+    #     else:
+    #         ideal_solution_data[i] = max(data[i])
+    # ideal_solution_data = np.array(ideal_solution_data)
+    # return ideal_solution_data
 
 
 def normalisation(data):
     # Normalisation
     normalised_data = data.copy()
-    for i in data.columns:
+    for i in range(1, len(data.columns.values)):
         column_sum_of_squares = 0
 
-        for j in data[i]:
-            column_sum_of_squares += j*j
+        for j in range(0, len(data)):
+            column_sum_of_squares += data.iloc[j, i]*data.iloc[j, i]
 
         column_sum_of_squares = column_sum_of_squares**0.5
 
-        for k in range(len(data[i])):
-            normalised_data[i][k] = data[i][k]/column_sum_of_squares
+        for k in range(0, len(data)):
+            normalised_data.iat[k, i] = (
+                data.iloc[k, i]/column_sum_of_squares)
 
     return normalised_data
 
@@ -158,9 +176,9 @@ def normalisation(data):
 def weighted_sum(normalised_data, weights):
     # Weighted sum
     weighted_sum_data = normalised_data.copy()
-    for i in normalised_data.columns:
-        for j in range(len(normalised_data[i])):
-            weighted_sum_data[i][j] = normalised_data[i][j]*weights[j]
+    for i in range(0, len(normalised_data.columns.values)-1):
+        for j in range(len(normalised_data)):
+            weighted_sum_data.iloc[j, i] *= weights[i]
 
     return weighted_sum_data
 
